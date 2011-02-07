fd23948716042b80c08d563662f16e8d574260fb
loianeg <loianeg@masteribm.br.ibm.com>
2011-02-07 14:55:21 +0000

    organized imports

--- src/com/loiane/data/EmployeeMapper.java
+++ src/com/loiane/data/EmployeeMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Case;
 import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.TypeDiscriminator;
 
